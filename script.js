5f57d7a640ed5027507014ce789a4f49dee38aa9
VedantKCSE <98043938+VedantKCSE@users.noreply.github.com>
2023-06-24 11:51:23 +0530

    commit

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-<<<<<<< Updated upstream
-
-=======
-// Transaction object constructor
-function Transaction(type, description, amount) {
-  this.type = type;
-  this.description = description;
-  this.amount = amount;
-}
-
-// Global variables
->>>>>>> Stashed changes
 let transactions = [];
 let currentBalance = 0;
 
-<<<<<<< Updated upstream
 
 function addTransaction(description, amount, type) {
   const transaction = {
@@ -21,13 +8,6 @@ function addTransaction(description, amount, type) {
     amount: parseFloat(amount),
     type
   };
-=======
-// Function to add a new transaction
-function addTransaction() {
-  const type = document.getElementById('type').value;
-  const description = document.getElementById('description').value;
-  const amount = parseFloat(document.getElementById('amount').value);
->>>>>>> Stashed changes
 
   if (description !== '' && !isNaN(amount)) {
     const transaction = new Transaction(type, description, amount);
@@ -38,28 +18,12 @@ function addTransaction() {
   }
 }
 
-<<<<<<< Updated upstream
 
 function deleteTransaction(index) {
   transactions.splice(index, 1);
 }
 
 
-=======
-// Function to update the transactions list
-function updateTransactionsList() {
-  const transactionsList = document.getElementById('transactions-list');
-  transactionsList.innerHTML = '';
-
-  transactions.forEach((transaction) => {
-    const li = document.createElement('li');
-    li.textContent = `${transaction.description} (${transaction.type}): ${transaction.amount}`;
-    transactionsList.appendChild(li);
-  });
-}
-
-// Function to update the current balance
->>>>>>> Stashed changes
 function updateBalance() {
   currentBalance = 0;
 
@@ -75,7 +39,6 @@ function updateBalance() {
   currentBalanceElement.textContent = currentBalance.toFixed(2);
 }
 
-<<<<<<< Updated upstream
 
 function displayTransactions() {
   const list = document.getElementById("list");
@@ -107,12 +70,3 @@ document.getElementById("transaction-form").addEventListener("submit", function(
 
   this.reset();
 });
-=======
-// Function to clear the form inputs
-function clearForm() {
-  document.getElementById('type').value = 'income';
-  document.getElementById('description').value = '';
-  document.getElementById('amount').value = '';
-}
-
->>>>>>> Stashed changes
