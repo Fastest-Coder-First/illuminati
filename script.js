5162df50d023ff5066fa97ae145713483979498c
VedantKCSE <98043938+VedantKCSE@users.noreply.github.com>
2023-06-24 11:59:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,84 @@
-// Transaction data
 let transactions = [];
+let balance = 0;
 
-// Function to add a new transaction
-function addTransaction(description, amount, type) {
+// Load transactions from localStorage on page load
+document.addEventListener('DOMContentLoaded', function() {
+  if (localStorage.getItem('transactions')) {
+    transactions = JSON.parse(localStorage.getItem('transactions'));
+    updateBalance();
+    updateTransactionsList();
+  }
+});
+
+function addTransaction() {
+  const description = document.getElementById('description').value;
+  const amount = parseFloat(document.getElementById('amount').value);
+  const type = document.getElementById('type').value;
+  
+  if (description.trim() === '' || isNaN(amount)) {
+    alert('Please enter valid values for description and amount.');
+    return;
+  }
+  
   const transaction = {
-    description,
-    amount: parseFloat(amount),
-    type
+    description: description,
+    amount: amount,
+    type: type
   };
-
+  
   transactions.push(transaction);
+  updateBalance();
+  updateTransactionsList();
+  saveTransactionsToLocalStorage();
+  
+  document.getElementById('description').value = '';
+  document.getElementById('amount').value = '';
 }
 
-// Function to delete a transaction
-function deleteTransaction(index) {
-  transactions.splice(index, 1);
-}
-
-// Function to update the balance
 function updateBalance() {
-  let balance = 0;
-
+  balance = 0;
+  
   transactions.forEach(transaction => {
-    if (transaction.type === "income") {
+    if (transaction.type === 'income') {
       balance += transaction.amount;
-    } else if (transaction.type === "expense") {
+    } else if (transaction.type === 'expense') {
       balance -= transaction.amount;
     }
   });
-
-  document.getElementById("balance-amount").textContent = balance.toFixed(2);
+  
+  const balanceElement = document.getElementById('balance');
+  balanceElement.textContent = 'Balance: $' + balance.toFixed(2);
+  
+  if (balance >= 0) {
+    balanceElement.style.color = 'green';
+  } else {
+    balanceElement.style.color = 'red';
+  }
 }
 
-// Function to display transactions
-function displayTransactions() {
-  const list = document.getElementById("list");
-  list.innerHTML = "";
-
+function updateTransactionsList() {
+  const transactionsList = document.getElementById('transactions');
+  transactionsList.innerHTML = '';
+  
   transactions.forEach((transaction, index) => {
-    const listItem = document.createElement("li");
+    const listItem = document.createElement('li');
     listItem.innerHTML = `
       <span class="${transaction.type}">${transaction.description}</span>
-      <span>${transaction.amount.toFixed(2)}</span>
+      <span>${transaction.type === 'income' ? '+' : '-'}$${transaction.amount.toFixed(2)}</span>
       <button onclick="deleteTransaction(${index})">Delete</button>
     `;
-
-    list.appendChild(listItem);
+    transactionsList.appendChild(listItem);
   });
 }
 
-// Event listener for transaction form submission
-document.getElementById("transaction-form").addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  const description = document.getElementById("description").value;
-  const amount = document.getElementById("amount").value;
-  const type = document.getElementById("type").value;
-
-  addTransaction(description, amount, type);
+function deleteTransaction(index) {
+  transactions.splice(index, 1);
   updateBalance();
-  displayTransactions();
+  updateTransactionsList();
+  saveTransactionsToLocalStorage();
+}
 
-  this.reset();
-});
+// Save transactions to localStorage
+function saveTransactionsToLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
